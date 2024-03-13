46b7d0accb7d0f13fd7a3af9dcd9dd3d6068836c
Laurentino <matheus.laurentino.ifpr@gmail.com>
2024-03-13 16:37:31 -0300

    add Category

---
+++ src/main/java/laurentino/workshop/services/CategoryService.java
@@ -0,0 +1,29 @@
+package laurentino.workshop.services;
+
+import laurentino.workshop.entities.Category;
+import laurentino.workshop.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    private CategoryRepository repository;
+
+    @Autowired
+    public CategoryService(CategoryRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        return obj.get();
+    }
+}
