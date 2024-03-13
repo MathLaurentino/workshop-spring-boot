26575cd8576f2333d791b90fd28bce24c88fb1a9
Laurentino <matheus.laurentino.ifpr@gmail.com>
2024-03-13 16:10:29 -0300

    add OrderStatus enum

--- src/main/java/laurentino/workshop/entities/Order.java
+++ src/main/java/laurentino/workshop/entities/Order.java
@@ -1,6 +1,7 @@
 package laurentino.workshop.entities;
 
 import jakarta.persistence.*;
+import laurentino.workshop.entities.enums.OrderStatus;
 
 import java.io.Serializable;
 import java.time.Instant;
@@ -16,6 +17,7 @@ public class Order implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Instant moment;
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -25,9 +27,10 @@ public class Order implements Serializable {
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -45,6 +48,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
